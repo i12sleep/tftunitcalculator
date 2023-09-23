465b6e3eb6b680282d383700805d97843a7f39ba
i12sleep <134366369+i12sleep@users.noreply.github.com>
2023-09-23 12:24:20 +1000

    added basic variables for html input

---
+++ index.js
@@ -0,0 +1,10 @@
+//this function needs to take care of calculations
+//for calculating the probabilities of getting a certain cost unit
+	
+let unit_cost;
+let gold;
+let wanted;
+
+function (input) {
+	
+}
