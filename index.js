91dafe65e60bada77f49aecc947d5be45ca86717
i12sleep <134366369+i12sleep@users.noreply.github.com>
2023-09-27 21:21:57 +1000

    updated stuff for project

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 //this function needs to take care of calculations
 //for calculating the probabilities of getting a certain cost unit
-	
-let unit_cost;
-let gold;
-let wanted;
+// what we want to do here is to have a function that calculates
+// the probability of getting a certain unit from the pool
 
-function (input) {
-	
+
+function calculate() {
+	let unit_cost = document.getElementById("unitCost");
+	let gold = document.getElementById("amntOfGold");
+	let wanted = document.getElementById("costOOP");
+	let req = document.getElementById("reqOOP");
+
+	//from these params we are able to calculate it
+	if (unit_cost === null || gold === null ||
+		wanted === null || req === null) {
+		return console.log("Please ")
+	}
 }
---
+++ index.ts
@@ -0,0 +1,3 @@
+// index.js but in ts to make sure i get the js functionality down
+// first before I create types
+//
